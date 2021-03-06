b5b8a50e10fa4e911a3e15e84540b1e78ede697d
KrijnS8 <krijn.schelvis@gmail.com>
2021-03-06 14:16:01 +0100

    Connection to oscPort

--- js/server.js
+++ js/server.js
@@ -6,8 +6,6 @@ let server = app.listen(3000);
 
 app.use(express.static('public'));
 
-console.log('The server is running');
-
 let socket = require('socket.io');
 
 let io = socket(server);
@@ -17,6 +15,14 @@ io.sockets.on('connection', newConnection);
 function newConnection(socket) {
 
     console.log('new connection: ' + socket.id);
+
+    let oscPort = new osc.WebSocketPort({
+        url: 'http://localhost:3001',
+        metadata: true
+    });
+    oscPort.open();
 }
+
+console.log('The server is running');
 
 // use socket.emit to send messages and socket.on to receive messages
